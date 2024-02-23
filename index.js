365980b5132be015496b987ad8812abd6a4b56d6
renato-av <renato.avela.17@gmail.com>
2024-02-22 23:17:50 -0500

    <feat>: adding commands

--- index.js
+++ index.js
@@ -1,19 +1,56 @@
+const { Client, GatewayIntentBits, Events, Collection } = require('discord.js')
+const dotenv = require('dotenv')
+const fs = require('node:fs')
+const path = require('node:path')
 
-    const Discord = require('discord.js');
-    const dotenv = require('dotenv')
-    
-    dotenv.config()
-    const TOKEN = process.env.TOKEN
+dotenv.config()
+const TOKEN = process.env.TOKEN
 
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+client.commands = new Collection()
 
-    const Client = new Discord.Client({
-        intents: 33027
-    })
+const foldersPath = path.join(__dirname, 'commands')
+const commandFolders = fs.readdirSync(foldersPath)
 
-  
-    Client.on('ready', async ( client ) => {
-         console.log('Estoy listo!')
-    })
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder)
+  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'))
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file)
+    const command = require(filePath)
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ('data' in command && 'execute' in command) {
+      client.commands.set(command.data.name, command)
+    } else {
+      console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`)
+    }
+  }
+}
 
+client.once(Events.ClientReady, readyClient => {
+  console.log(`Logged in as ${readyClient.user.tag}!`)
+})
 
-    Client.login(TOKEN)
+client.on(Events.InteractionCreate, async interaction => {
+  if (!interaction.isChatInputCommand()) return
+
+  const command = interaction.client.commands.get(interaction.commandName)
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`)
+    return
+  }
+
+  try {
+    await command.execute(interaction)
+  } catch (error) {
+    console.error(error)
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true })
+    } else {
+      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
+    }
+  }
+})
+
+client.login(TOKEN)
