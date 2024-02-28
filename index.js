4b0d7f37b393d6df57dd078dab99d06dc6dbe40b
renato-av <renato.avela.17@gmail.com>
2024-02-28 10:38:19 -0500

    prueba

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-const { Client, GatewayIntentBits, Events, Collection, EmbedBuilder } = require('discord.js')
-const dotenv = require('dotenv')
-const fs = require('node:fs')
-const path = require('node:path')
+import { Client, GatewayIntentBits, Events, Collection, EmbedBuilder } from 'discord.js'
+import { config } from 'dotenv'
+import { readdirSync } from 'node:fs'
+import { join } from 'node:path'
 
-dotenv.config()
+config()
 const TOKEN = process.env.TOKEN
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages
+  ]
+})
 client.commands = new Collection()
 
-const foldersPath = path.join(__dirname, 'commands')
-const commandFolders = fs.readdirSync(foldersPath)
+const foldersPath = join(__dirname, 'commands')
+const commandFolders = readdirSync(foldersPath)
 
 for (const folder of commandFolders) {
-  const commandsPath = path.join(foldersPath, folder)
-  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'))
+  const commandsPath = join(foldersPath, folder)
+  const commandFiles = readdirSync(commandsPath).filter(file => file.endsWith('.js'))
   for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file)
+    const filePath = join(commandsPath, file)
     const command = require(filePath)
     // Set a new item in the Collection with the key as the command name and the value as the exported module
     if ('data' in command && 'execute' in command) {
@@ -32,28 +37,30 @@ client.once(Events.ClientReady, readyClient => {
 })
 
 client.on(Events.MessageCreate, message => {
-  if (message.content.startsWith('!enviarEmbed')) {
-    // Extraer el contenido del mensaje JSON del comando
-    const content = message.content.substring('!enviarEmbed'.length).trim()
-    try {
-      const embedData = JSON.parse(content)
+  const channel = message.channel
+  channel.send('Hello, world!')
+  // if (message.content.startsWith('!enviarEmbed')) {
+  //   // Extraer el contenido del mensaje JSON del comando
+  //   const content = message.content.substring('!enviarEmbed'.length).trim()
+  //   try {
+  //     const embedData = JSON.parse(content)
 
-      const embed = new EmbedBuilder()
-        .setTitle(embedData.title)
-        .setDescription(embedData.description)
-        .setColor(embedData.color)
+  //     const embed = new EmbedBuilder()
+  //       .setTitle(embedData.title)
+  //       .setDescription(embedData.description)
+  //       .setColor(embedData.color)
 
-      embedData.fields.forEach(field => {
-        embed.addField(field.name, field.value, field.inline)
-      })
+  //     embedData.fields.forEach(field => {
+  //       embed.addField(field.name, field.value, field.inline)
+  //     })
 
-      // Enviar el mensaje embed al canal de texto
-      const channel = message.channel
-      channel.send(embed)
-    } catch (error) {
-      message.reply('Error al analizar el JSON')
-    }
-  }
+  //     // Enviar el mensaje embed al canal de texto
+  //     const channel = message.channel
+  //     channel.send(embed)
+  //   } catch (error) {
+  //     message.reply('Error al analizar el JSON')
+  //   }
+  // }
 })
 
 client.on(Events.InteractionCreate, async interaction => {
